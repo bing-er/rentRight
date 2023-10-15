46e0d670c4804b657e15f9f4d386dc9fdc3ec8a1
KeYang <kyang72@my.bcit.ca>
2023-10-15 16:42:19 -0700

    adding spinner functionality

--- app/src/main/java/my/bcit/rentright/LoginActivity.kt
+++ app/src/main/java/my/bcit/rentright/LoginActivity.kt
@@ -2,10 +2,20 @@ package my.bcit.rentright
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val spinner: Spinner = findViewById(R.id.userRole)
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.user_type, android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
     }
 }
